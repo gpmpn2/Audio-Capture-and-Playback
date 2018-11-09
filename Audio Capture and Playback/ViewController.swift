30938dd3598f57dbd0bcc92249f8091fb4c10dd4
Grant Maloney <grantmaloney@mwc-033126.dhcp.missouri.edu>
2018-11-09 12:40:59 -0600

    Final commit of Audio Capture and Playback

--- Audio Capture and Playback/ViewController.swift
+++ Audio Capture and Playback/ViewController.swift
@@ -7,14 +7,151 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var playButton: UIBarButtonItem!
+    @IBOutlet weak var recordButton: UIBarButtonItem!
+    @IBAction func playButtonAction(_ sender: Any) {
+        playBack()
+    }
+    @IBAction func recordButtonAction(_ sender: Any) {
+        if audioRecorder == nil {
+            startRecording()
+        } else {
+            finishRecording(success: true)
+        }
+    }
+    
+    var recordingSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        recordButton.image = UIImage(named: "record")
+        
+        playButton.image = UIImage(named: "play")
+        
+        recordingSession = AVAudioSession.sharedInstance()
+        
+        label.text = "Welcome to Audio Capture"
+        
+        do {
+            try recordingSession.setCategory(.playAndRecord, mode: .default)
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission() { [unowned self] allowed in
+                DispatchQueue.main.async {
+                    if allowed {
+                        if self.checkSavedAudioFile() == nil {
+                            self.playButton.isEnabled = false
+                        }
+                    } else {
+                        self.playButton.isEnabled = false
+                        self.recordButton.isEnabled = false
+                    }
+                }
+            }
+        } catch {
+            createAlert(message: "Failed to record audio")
+        }
+    }
+    
+    func startRecording() {
+        let settings = [
+            AVEncoderAudioQualityKey: AVAudioQuality.min.rawValue,
+            AVEncoderBitRateKey: 16,
+            AVNumberOfChannelsKey: 2,
+            AVSampleRateKey: 44100.0
+        ] as [String : Any]
+        
+        do {
+            if let audioFile = checkSavedAudioFile() {
+                audioRecorder = try AVAudioRecorder(url: audioFile, settings: settings)
+            }
+            audioRecorder.delegate = self
+            audioRecorder.record()
+            label.text = "Recording audio..."
+            recordButton.image = UIImage(named: "stop")
+            playButton.isEnabled = false
+        } catch {
+            finishRecording(success: false)
+        }
+    }
+    
+    func finishRecording(success: Bool) {
+        audioRecorder.stop()
+        audioRecorder = nil
+        
+        if success {
+            label.text = "Successfully recorded audio"
+            recordButton.image = UIImage(named: "record")
+            playButton.isEnabled = true
+        } else { //Recording failed
+            createAlert(message: "Recording failed")
+            recordButton.image = UIImage(named: "record")
+        }
     }
 
-
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if !flag {
+            finishRecording(success: false)
+        }
+    }
+    
+    func playBack() {
+        if let player = player {
+            let time = String(format: "%.2f", player.currentTime)
+            if player.isPlaying {
+                player.pause()
+                label.text = "Paused audio at \(time) seconds"
+                recordButton.isEnabled = true
+                playButton.image = UIImage(named: "play")
+                return
+            } else {
+                label.text = "Resuming audio at \(time) seconds"
+                player.play()
+                recordButton.isEnabled = false
+                playButton.image = UIImage(named: "pause")
+                return
+            }
+        }
+        
+        do {
+            if let audioFile = checkSavedAudioFile() {
+                player = try AVAudioPlayer(contentsOf: audioFile)
+            }
+            player?.numberOfLoops = 0
+            player?.prepareToPlay()
+            player?.delegate = self
+            player?.play()
+            label.text = "Playing audio"
+            recordButton.isEnabled = false
+            playButton.image = UIImage(named: "pause")
+        } catch {
+            createAlert(message: "Unable to play audio")
+        }
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        playButton.image = UIImage(named: "play")
+        recordButton.isEnabled = true
+        self.player = nil
+        label.text = "Audio finished"
+    }
+    
+    func createAlert(message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        self.present(alert, animated: true)
+    }
+    
+    func checkSavedAudioFile() -> URL? {
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return path[0].appendingPathComponent("recording.caf")
+    }
 }
 
